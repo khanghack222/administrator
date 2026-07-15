812d6a1d9b329ae49b99a112331bf6d831f6ca07
khanghack222 <kieuxuan257@gmail.com>
2026-07-15 21:04:02 +0700

    Delete mail directory

--- mail/edu-create.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo [edu-create] tao mail → acc/N.json + acc/latest.json
-node getedumail-auto.mjs --no-open %*
-echo.
-echo Xong. Extension 1b (edu-bridge) | menu [2] login.
-pause
--- mail/edu-login.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Login form Chrome (email+pass tu getedumail-latest.json)
-node getedumail-auto.mjs --login %*
